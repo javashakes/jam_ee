6d34f8f71e6b29c6b19bb4996bd0c7a139b2bbd1
Matthew Kirkpatrick <mk@bidwrangler.com>
2020-09-04 16:33:04 -0400

    updated namespace

--- jam/config.php
+++ jam/config.php
@@ -19,7 +19,7 @@ if ( ! defined('JAM_NAME'))
     define('JAM_DOCS',              'https://github.com/javashakes/jam_ee');
     define('JAM_DESC',              '<p><strong>An ExpressionEngine Plugin that exposes PHP\'s time(), date(), and mktime() to EE templates.</strong></p>Documentation and Usage: <a href="' . JAM_DOCS . '" target="_blank">' . JAM_DOCS . '</a>');
     define('JAM_PACKAGE',           'jam');
-    define('JAM_NAMESPACE',         'JustAMinute');
+    define('JAM_NAMESPACE',         'javashakes\JustAMinute');
     define('JAM_CLASS_NAME',        'Jam');
 }
 
